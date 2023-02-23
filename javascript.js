7b62547a46a4ce2c5c620e817eb3f18d55dca608
Cpt-Cooper <viviccadri@outlook.com>
2023-02-23 20:34:19 -0300

    Changing de logic and add game function

--- javascript.js
+++ javascript.js
@@ -12,15 +12,29 @@ function getComputerChoice() {
             break;
     }
 }
-var computerSelection = getComputerChoice()
-var playerSelection = "paper"
 function playRound(computerSelection, playerSelection) {
-    if (playerSelection == "paper" && computerSelection == "rock") {
-        return "You Win! Papers beats Rock"
-    } else if (playerSelection == "paper" && computerSelection == "scissors") {
-        return "You Lose! Scissors beats Paper"
-    } else {
+    if (computerSelection === playerSelection){
         return "Draw"
+    } else if (playerSelection == "rock" && computerSelection == "paper"){
+        return "You Lose! Paper beats Rock"
+    } else if (playerSelection == "rock" && computerSelection == "scissors"){
+        return "You Win! Rock beats Scissors"
+    } else if (playerSelection == "paper" && computerSelection == "scissors"){
+        return "You Lose! Scissors beats Paper"
+    } else if (playerSelection == "paper" && computerSelection == "rock"){
+        return "You Win! Paper beats Rock"
+    } else if (playerSelection == "scissors" && computerSelection == "rock"){
+        return "You Lose! Rock beats scissors"
+    } else if (playerSelection == "scissors" && computerSelection == "paper"){
+        return "You Win! Scissors beats Paper"
     }
 }
-console.log(playRound(computerSelection, playerSelection))
+function game(){
+    for (let i = 0; i < 5; i++){
+        let playerSelection = prompt("What's your choice?: (rock, paper, scissors)")
+        playerSelection.toLowerCase()
+        let computerSelection = getComputerChoice()
+        console.log(playRound(computerSelection, playerSelection))
+    }
+}
+game()
