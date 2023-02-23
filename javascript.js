1d8e63ff3fcca98bdc19a4fd1ea60730c09006a3
Cpt-Cooper <viviccadri@outlook.com>
2023-02-22 21:59:36 -0300

    Computer Random Choice

--- javascript.js
+++ javascript.js
@@ -1,1 +1,15 @@
-console.log("Hello, World!")
+function getComputerChoice() {
+    let randomInt = Math.floor(Math.random() * 3)
+    switch (randomInt) {
+        case 0:
+            return "rock";
+            break;
+        case 1:
+            return "paper";
+            break;
+        case 2:
+            return "scissors";
+            break;
+    }
+}
+
